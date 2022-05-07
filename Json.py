ab32eb51192b2ad9283f033ddfad7a3ada94c870
vva <sage>
2022-05-07 10:56:30 +0200

    Added JSON data saver/loader

---
+++ Json.py
@@ -0,0 +1,28 @@
+# 
+# data = {"src": self.id, "dest": dest,"msg":obj}
+# json_dump = json.dumps(data)
+# msg = json.loads(msg)
+import json
+import jsons
+
+class JSONTest():
+    egesz = 5
+    lebego = 3.1
+    szoveg = "proba"
+    lista = ["1", 2, 3.0]
+    hamis = False
+    igaz = True
+
+def toJSON(src, fname, mode):
+    contents = jsons.dumps(src)
+    fl = open(fname, mode)
+    if mode != "r":
+        fl.write(contents)
+    fl.close()
+
+if __name__ == "__main__":
+    t = JSONTest()
+    toJSON(t, 'test.json', 'w')
+    fl = open('test.json', 'r')
+    print(json.load(fl))
+
